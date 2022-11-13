8865292bb05d49531b89d172ec63fb901c751f4f
Joao Junior <joaojunior1205@gmail.com>
2022-11-13 12:49:31 -0300

    fix: remove migrations and dependencies typeorm

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`Server running in port ${port}`));
+app.listen(port, () => {
+    console.log(`Server running in port ${port}`)
+});
